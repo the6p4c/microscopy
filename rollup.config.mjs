baae073fc000431cdc81241cbc4fb8c83b151955
the6p4c <me@doggirl.gay>
2023-10-01 02:04:48 +1000

    add github actions build workflow

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,21 @@
+import pkg from './package.json' assert { type: 'json' };
 import nodeResolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 const isProduction = process.env.NODE_ENV === 'production';
 
-function unpkg(name: string, version: string, path: string): string {
+function unpkg(name, path) {
+  if (!(name in pkg.dependencies)) {
+    throw `couldn't find ${name} in package.json dependencies`;
+  }
+
+  const version = pkg.dependencies[name];
+  // catch a couple of obvious problems
+  if (/^><~\^\*/.test(version)) {
+    throw `version ${version} for ${name} doesn't look good`;
+  }
+
   return `https://unpkg.com/${name}@${version}/${path}`;
 }
 
@@ -22,19 +30,17 @@ export default {
       'react-dom/client': 'ReactDOM',
     },
   },
-  external: (id: string) => /^react(-dom)?/.test(id),
+  external: (id) => /^react(-dom)?/.test(id),
   plugins: [
     nodeResolve({ extensions: ['.js', '.ts', '.tsx'] }),
     typescript(),
     replace({
       preventAssignment: true,
       'process.env.NODE_ENV': process.env.NODE_ENV,
-      '__react__': unpkg(
-        'react', React.version,
+      '__react__': unpkg('react',
         `umd/react.${isProduction ? 'production.min.js' : 'development.js'}`
       ),
-      '__react-dom__': unpkg(
-        'react-dom', ReactDOM.version,
+      '__react-dom__': unpkg('react-dom',
         `umd/react-dom.${isProduction ? 'production.min.js' : 'development.js'}`
       ),
     }),
